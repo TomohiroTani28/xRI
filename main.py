b5eb8444a54004dffdbd6397079a61f5e4851653
TomohiroTani28 <amlinks0621@gmail.com>
2024-03-30 16:45:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 from dotenv import load_dotenv
 from ai.content_generator import generate_content  # async_content_generatorから変更
-from api.async_x_api_client import post_to_x
+from api.x_api_client import post_to_x
 from db.async_database import check_and_update_post_history
 from utils.helpers import setup_logging
 
