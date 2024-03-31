f90bf6678cb6b22543320030819b49855ce934b8
TomohiroTani28 <amlinks0621@gmail.com>
2024-03-31 13:31:43 +0900

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -19,16 +19,22 @@ def format_datetime(date_time=None, date_format="%Y-%m-%d %H:%M:%S"):
         date_time = datetime.now()
     return date_time.strftime(date_format)
 
+def split_text_into_sentences(text):
+    """テキストを文に分割します。"""
+    sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|！)\s', text)
+    return sentences
+
 def split_text_into_posts(text, max_length=280):
     """テキストを自然な区切りで分割し、各ポストが280文字を超えないようにします。"""
+    sentences = split_text_into_sentences(text)
     posts = []
-    while text:
-        if len(text) <= max_length:
-            posts.append(text)
-            break
-        cut_off = text.rfind(' ', 0, max_length)
-        if cut_off == -1:  # スペースが見つからない場合は、max_lengthで強制的に分割
-            cut_off = max_length
-        posts.append(text[:cut_off])
-        text = text[cut_off:].strip()
+    post = ""
+    for sentence in sentences:
+        if len(post + sentence) + 1 <= max_length:  # +1 for space
+            post += sentence + " "
+        else:
+            posts.append(post.strip())
+            post = sentence + " "
+    if post:
+        posts.append(post.strip())
     return posts
